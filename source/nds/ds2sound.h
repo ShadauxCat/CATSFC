1beb671de1d39964ad3453e28fd48a117046a8cb
Nebuleon Fumika <nebuleon@alakazam>
2013-01-08 15:04:53 -0500

    Configurable interrupt interval for the sound timer. Right now, only 22050 Hz audio at 2000-microsecond intervals works.

--- source/nds/ds2sound.h
+++ source/nds/ds2sound.h
@@ -1,12 +1,11 @@
 // The sound buffer sizes used on the DS2's side, for each value of
 // Settings.SoundPlaybackRate.
-#define DS2_BUFFER_SIZE_1 256
-#define DS2_BUFFER_SIZE_2 256
-#define DS2_BUFFER_SIZE_3 256
-#define DS2_BUFFER_SIZE_4 512
-#define DS2_BUFFER_SIZE_5 512
-#define DS2_BUFFER_SIZE_6 1024
-#define DS2_BUFFER_SIZE_7 1024
+#define DS2_BUFFER_SIZE_4  512 /* tested working */
+// Others don't work, or don't work well.
+
+// In microseconds.
+#define INTERRUPT_TIME_4  2000 /* tested working */
+// Others don't work, or don't work well.
 
 // The sampling rate for the sound, in Hz, for each value of
 // Settings.SoundPlaybackRate.
@@ -19,6 +18,7 @@
 #define SND_SAMPLE_RATE_7 48000
 
 // Settings in use. The number should match in all three settings.
+#define INTERRUPT_TIME    INTERRUPT_TIME_4
 #define DS2_BUFFER_SIZE   DS2_BUFFER_SIZE_4
 #define SND_SAMPLE_RATE   SND_SAMPLE_RATE_4
 #define SNES9X_SRATE_ID   4
